2472e110446140cf99e9a977c1f6bc25dbb50eb9
石阪友貴 <ishizakatomoki@ishizakatomotakanoMacBook-Air.local>
2025-04-08 06:38:38 +0900

    Netlifyのデプロイ設定とルーティング設定を修正

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,17 @@ import App from './App'
 import './index.css'
 
 // ルーティングの設定
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />
+    }
+  ],
   {
-    path: '/',
-    element: <App />
+    basename: import.meta.env.BASE_URL
   }
-])
+)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
